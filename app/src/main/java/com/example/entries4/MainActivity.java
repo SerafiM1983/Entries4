a339b4be1f7ecf6af75c90b1fd402b0b0c542bd1
k.datchenko <nsk-54ru@mail.ru>
2023-07-10 22:17:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/entries4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.entries4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.entries4.databinding.ActivityMainBinding;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater()); // находим все элементы экрана
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Действия, выполняемые при нажатии кнопки
+                Toast.makeText(MainActivity.this, "enter fab", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
